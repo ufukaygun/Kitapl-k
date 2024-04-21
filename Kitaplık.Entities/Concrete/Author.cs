f40c52ffa4dc9da6a53c5a21fc681826ac62e58b
ufukaygun <u.aygun869@gmail.com>
2024-04-21 16:49:44 +0300

    Kitaplık.Entities.Concrete de Author classı oluşturuldu

---
+++ Kitaplık.Entities/Concrete/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitaplık.Entities.Concrete
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDelete { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+
+
+    }
